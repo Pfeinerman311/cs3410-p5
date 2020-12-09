a64552087b1678f0b5da3d2894d211d64f5485bc
Neil Manish Madhavani <nmm85@en-ci-cisugcl14..coecis.cornell.edu>
2020-12-09 17:55:55 -0500

    fixed seg fault

--- p5-cache/cache.c
+++ p5-cache/cache.c
@@ -7,18 +7,20 @@
 #include "cache_stats.h"
 #include "print_helpers.h"
 
-cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol){
+cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t protocol)
+{
   cache_t *cache = malloc(sizeof(cache_t));
+  cache->stats = make_cache_stats();
 
-  cache->capacity = capacity;      // in Bytes
-  cache->block_size = block_size;  // in Bytes
-  cache->assoc = assoc;            // 1, 2, 3... etc.
+  cache->capacity = capacity;     // in Bytes
+  cache->block_size = block_size; // in Bytes
+  cache->assoc = assoc;           // 1, 2, 3... etc.
 
   // FIX THIS CODE!
   // first, correctly set these 5 variables. THEY ARE ALL WRONG
   // note: you may find math.h's log2 function useful
-  cache->n_total_cache_line = capacity/block_size;
-  cache->n_set = capacity/(assoc * block_size);
+  cache->n_total_cache_line = capacity / block_size;
+  cache->n_set = capacity / (assoc * block_size);
   cache->n_offset_bit = log2(block_size);
   cache->n_index_bit = log2(cache->n_set);
   cache->n_tag_bit = 32 - (cache->n_offset_bit) - (cache->n_index_bit);
@@ -28,22 +30,22 @@ cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t pro
   // - for each element in the array, malloc another array with n_col
   // FIX THIS CODE!
 
-  cache->lines = 
-	(cache_line_t **) malloc((cache->n_set) * sizeof(cache_line_t *));
-  	for (int i = 0; i < (cache->n_set); i++) {
-		(cache->lines)[i] = malloc(assoc * sizeof(cache_line_t));
-	}
-
-
-
+  cache->lines =
+      malloc((cache->n_set) * sizeof(cache_line_t *));
+  for (int i = 0; i < (cache->n_set); i++)
+  {
+    (cache->lines)[i] = malloc(assoc * sizeof(cache_line_t));
+  }
 
   cache->lru_way = malloc((cache->n_set) * sizeof(int));
 
   // initializes cache tags to 0, dirty bits to false,
   // state to INVALID, and LRU bits to 0
   // FIX THIS CODE!
-  for (int i = 0; i < (cache->n_set); i++) {
-    for (int j = 0; j < assoc; j++) {
+  for (int i = 0; i < (cache->n_set); i++)
+  {
+    for (int j = 0; j < assoc; j++)
+    {
       (cache->lines[i][j]).tag = 0;
       (cache->lines[i][j]).dirty_f = 0;
       (cache->lines[i][j]).state = INVALID;
@@ -54,21 +56,23 @@ cache_t *make_cache(int capacity, int block_size, int assoc, enum protocol_t pro
   return cache;
 }
 
-unsigned long get_cache_tag(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_tag(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return addr >> (32 - (cache->n_tag_bit));
 }
 
-unsigned long get_cache_index(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_index(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return (addr >> cache->n_offset_bit) & ~(~0 << cache->n_index_bit);
 }
 
-unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
+unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr)
+{
   // FIX THIS CODE!
   return (addr >> (cache->n_offset_bit)) << (cache->n_offset_bit);
 }
-
 
 /* this method takes a cache, an address, and an action
  * it proceses the cache access. functionality in no particular order: 
@@ -78,22 +82,29 @@ unsigned long get_cache_block_addr(cache_t *cache, unsigned long addr) {
  * return true if there was a hit, false if there was a miss
  * Use the "get" helper functions above. They make your life easier.
  */
-bool access_cache(cache_t *cache, unsigned long addr, enum action_t action) {
+bool access_cache(cache_t *cache, unsigned long addr, enum action_t action)
+{
   // FIX THIS CODE!
   unsigned long tag = get_cache_tag(cache, addr);
   unsigned long index = get_cache_index(cache, addr);
-  for(int a = 0; a < cache->assoc; a++) {
-	  if(tag == cache->lines[index][a].tag) {
-		  if (action == STORE) {
-			  cache->lines[index][a].dirty_f = 1;
-			  cache->lines[index][a].state = VALID;
-		  }
-		  return true;
-	  }
+  for (int a = 0; a < cache->assoc; a++)
+  {
+    if (tag == cache->lines[index][a].tag)
+    {
+      if (action == STORE)
+      {
+        cache->lines[index][a].dirty_f = 1;
+        cache->lines[index][a].state = VALID;
+      }
+      update_stats(cache->stats, true, false, false, action);
+      return true;
+    }
   }
   cache->lines[index][cache->lru_way[index]].tag = tag;
-  if (action == STORE) {
-	  cache->lines[index][cache->lru_way[index]].dirty_f = 1;
+  if (action == STORE)
+  {
+    cache->lines[index][cache->lru_way[index]].dirty_f = 1;
   }
+  update_stats(cache->stats, false, false, false, action);
   return false;
 }
